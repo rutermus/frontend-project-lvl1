f2a613e860d1cf7988193dd6c377cc7d5270d945
termus <rutermus@gmail.com>
2022-12-08 14:03:50 +0300

    created const with content, got the user name and created a greeting by name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import { getAnswer } from '../src/cli.js';
+
+const mainGreeting = `Welcome to the Brain Games!`;
+const userNameQuestion = `May I have your name? `;
+const userGreetingPhrase = `Hello`;
+
+console.log(mainGreeting);
+const userName = getAnswer(userNameQuestion);
+
+console.log(`${userGreetingPhrase}, ${userName}!`);
