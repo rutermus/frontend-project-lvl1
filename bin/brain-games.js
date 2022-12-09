ee7effd9c82ea45102958edc816b05c85f9d1d24
termus <rutermus@gmail.com>
2022-12-09 17:18:16 +0300

    fix eslint errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,9 +2,9 @@
 
 import { getAnswer } from '../src/cli.js';
 
-const mainGreeting = `Welcome to the Brain Games!`;
-const userNameQuestion = `May I have your name? `;
-const userGreetingPhrase = `Hello`;
+const mainGreeting = 'Welcome to the Brain Games!';
+const userNameQuestion = 'May I have your name? ';
+const userGreetingPhrase = 'Hello';
 
 console.log(mainGreeting);
 const userName = getAnswer(userNameQuestion);
