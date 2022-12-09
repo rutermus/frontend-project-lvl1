1626da84e38d3e759f7972b24d8b31267cab62e7
termus <rutermus@gmail.com>
2022-12-09 17:19:40 +0300

    fix eslint errors

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const getAnswer = (question) => {
-    const answer = readlineSync.question(question);
-    return answer;
+  const answer = readlineSync.question(question);
+  return answer;
 };
