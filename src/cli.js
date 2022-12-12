07b6872d54f7fcf13ec34853e990fdb0a45859f2
termus <rutermus@gmail.com>
2022-12-12 12:16:48 +0300

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,12 @@
 import readlineSync from 'readline-sync';
 
-export const getAnswer = (question) => {
-  const answer = readlineSync.question(question);
-  return answer;
+export const mainGreeting = 'Welcome to the Brain Games!';
+export const userNameQuestion = 'May I have your name?';
+export const userGreetingPhrase = 'Hello';
+
+export const brainGamesGreeting = (greetPhrase, helloUserPhrase) => {
+  console.log(greetPhrase);
+  const userName = readlineSync.question(`${userNameQuestion} `);
+  console.log(`${helloUserPhrase}, ${userName}!`);
+  return userName;
 };
