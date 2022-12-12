e1ee150af5a5df7d825ad39fdc1202944c3d74e8
termus <rutermus@gmail.com>
2022-12-12 12:17:32 +0300

    fix brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-import { getAnswer } from '../src/cli.js';
+import { brainGamesGreeting, mainGreeting, userGreetingPhrase } from '../src/cli.js';
 
-const mainGreeting = 'Welcome to the Brain Games!';
-const userNameQuestion = 'May I have your name? ';
-const userGreetingPhrase = 'Hello';
-
-console.log(mainGreeting);
-const userName = getAnswer(userNameQuestion);
-
-console.log(`${userGreetingPhrase}, ${userName}!`);
+brainGamesGreeting(mainGreeting, userGreetingPhrase);
