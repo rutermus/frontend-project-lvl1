8e9d4ed4566de5a408ff21f9eed2e5cf1d190172
termus <rutermus@gmail.com>
2022-12-13 17:44:46 +0300

    add brain-progression.js and make it executable

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { brainGamesGreeting } from '../../src/cli.js';
+import { userNameQuestion, brainProgressionGame } from '../../src/index.js';
+
+const userName = brainGamesGreeting(userNameQuestion);
+
+brainProgressionGame(userName);
